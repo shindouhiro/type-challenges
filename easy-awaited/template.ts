f7351946c3e2e4c93b33dc55379370f94a5f5614
Hikaru <shindouhiro@gmail.com>
2024-04-22 15:11:37 +0800

    feat: awaited 使用PromiseLike

--- easy-awaited/template.ts
+++ easy-awaited/template.ts
@@ -1,4 +1,12 @@
-type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X>
-  ? X extends Promise<unknown> ? MyAwaited<X> : X
-  : never
+// type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X>
+//   ? X extends Promise<unknown>
+//   ? MyAwaited<X> : X
+//   : never
+
+type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U>
+  ? U extends PromiseLike<any>
+  ? MyAwaited<U>
+  : U
+  : never;
+
 
